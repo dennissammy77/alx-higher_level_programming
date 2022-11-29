d6defe310308564e5922eb37a047b812ad42e1bb
dennissammy77 <dennissammy77@gmail.com>
2022-11-29 13:30:13 +0300

    changed bash script

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,2 @@
-#!/bin/python
+#!/usr/bin/python3
 print("Programming is like building a multilingual puzzle\n")
--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
-#!/bin/python
+#!/usr/bin/python3
 number = 98
 print(f"{number:d} Battery street\n")
