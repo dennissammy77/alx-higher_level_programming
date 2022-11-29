33a708971934dba85eaa39cd0ad0096ee5ca7a92
dennissammy77 <dennissammy77@gmail.com>
2022-11-29 16:48:42 +0300

    if else loops functions

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - inserts number into a sorted linked list
+ * @head: pointer to head of linked list.
+ * @number: number to be inserted.
+ * Return: NULL if function fails otherwise a pointer to new node.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) >= 97 and ord <= 122:
+        return True
+    else:
+        return False
